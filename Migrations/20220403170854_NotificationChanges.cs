c28ae350bfb79085c01f6bbf7fbfe0246f635664
Anjali0038 <anjalilajna38@gmail.com>
2022-04-03 22:55:21 +0545

    NotificationChanges

---
+++ Migrations/20220403170854_NotificationChanges.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BloodKoshh.Migrations
+{
+    public partial class NotificationChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notifications",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notifications", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Watchlists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EventId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BloodKoshhUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Watchlists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Watchlists_AspNetUsers_BloodKoshhUserId",
+                        column: x => x.BloodKoshhUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Watchlists_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserNotifications",
+                columns: table => new
+                {
+                    NotificationId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NotificationId1 = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BloodKoshhUserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    IsRead = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserNotifications", x => x.NotificationId);
+                    table.ForeignKey(
+                        name: "FK_UserNotifications_AspNetUsers_BloodKoshhUserId",
+                        column: x => x.BloodKoshhUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_UserNotifications_Notifications_NotificationId1",
+                        column: x => x.NotificationId1,
+                        principalTable: "Notifications",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotifications_BloodKoshhUserId",
+                table: "UserNotifications",
+                column: "BloodKoshhUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserNotifications_NotificationId1",
+                table: "UserNotifications",
+                column: "NotificationId1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Watchlists_BloodKoshhUserId",
+                table: "Watchlists",
+                column: "BloodKoshhUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Watchlists_EventId",
+                table: "Watchlists",
+                column: "EventId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserNotifications");
+
+            migrationBuilder.DropTable(
+                name: "Watchlists");
+
+            migrationBuilder.DropTable(
+                name: "Notifications");
+        }
+    }
+}
